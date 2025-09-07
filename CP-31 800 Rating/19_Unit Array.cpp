a502e173ca94f5f9f9b69772ea975f64d3b414dc
Ajithkumar-ak1 <ajithkumar.proff@gmail.com>
2025-09-07 19:34:16 +0530

    Solved 19_Unit Array.cpp

---
+++ CP-31 800 Rating/19_Unit Array.cpp
@@ -0,0 +1,21 @@
+//Solution Date 07/09/25
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+     int tc;
+     cin>>tc;
+     while(tc--){
+        int n;
+        cin>>n;
+        int neg = 0, pos = 0;
+        for(int i=0;i<n;i++){
+            int x; cin>>x;
+            if(x==1) pos++;
+            else neg++;
+        }
+        int sum = pos-neg, need = 0;
+        if(sum<0) need = (abs(sum)+1)/2;
+        neg-=need;
+        cout<<need+(neg%2)<<endl;
+    }    
+}
